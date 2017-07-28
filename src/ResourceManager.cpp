bc4e15849b21ddc5663e150c28a9299425a4fea4
Kajak Games KesäJamit 2017 <extKesaJamit@kamit.fi>
2017-07-28 22:02:10 +0300

    Continued resourceManager. Can be called globally.

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -5,39 +5,42 @@
 namespace jam {
 	sf::Texture& ResourceManager::GetTexture(const std::string& filePath)
 	{
-		if (textureFiles.find(filePath) == textureFiles.end()) {
-			std::cout << "Loading " + filePath + "..." << std::endl;
-			sf::Texture& texture = textureFiles[filePath];
-			if (!texture.loadFromFile(filePath))
+		std::string assetPath = "assets/Textures/" + filePath;
+		if (textureFiles.find(assetPath) == textureFiles.end()) {
+			std::cout << "Loading " + assetPath + "..." << std::endl;
+			sf::Texture& texture = textureFiles[assetPath];
+			if (!texture.loadFromFile(assetPath))
 			{
 				assert(false);
 			}
-			std::cout << "Loading " + filePath + " complete!" << std::endl;
+			std::cout << "Loading " + assetPath + " complete!" << std::endl;
 			return texture;
 		}
 	}
 
 	sf::SoundBuffer& ResourceManager::GetSoundBuffer(const std::string& filePath)
 	{
-		std::cout << "Loading " + filePath + "..." << std::endl;
-		sf::SoundBuffer& soundBuff = audioFiles[filePath];
-		if (!soundBuff.loadFromFile(filePath))
+		std::string assetPath = "assets/Audio/" + filePath;
+		std::cout << "Loading " + assetPath + "..." << std::endl;
+		sf::SoundBuffer& soundBuff = audioFiles[assetPath];
+		if (!soundBuff.loadFromFile(assetPath))
 		{
 			assert(false);
 		}
-		std::cout << "Loading " + filePath + " complete!" << std::endl;
+		std::cout << "Loading " + assetPath + " complete!" << std::endl;
 		return soundBuff;
 	}
 
 	sf::Font& ResourceManager::GetFont(const std::string& filePath)
 	{
-		std::cout << "Loading " + filePath + "..." << std::endl;
-		sf::Font font = fontFiles[filePath];
-		if (!font.loadFromFile(filePath))
+		std::string assetPath = "assets/Fonts/" + filePath;
+		std::cout << "Loading " + assetPath + "..." << std::endl;
+		sf::Font font = fontFiles[assetPath];
+		if (!font.loadFromFile(assetPath))
 		{
 			assert(false);
 		}
-		std::cout << "Loading " + filePath + " complete!" << std::endl;
+		std::cout << "Loading " + assetPath + " complete!" << std::endl;
 		return font;
 	}
 }
--- src/Scenes/GameScene.cpp
+++ src/Scenes/GameScene.cpp
@@ -1,11 +1,12 @@
 #include <Jam/Scenes/GameScene.hpp>
+#include <Jam/Instance.hpp>
 
 namespace jam
 {
   GameScene::GameScene(Instance& ins)
     : Scene(ins)
   {
-
+	  // sf::Texture testText = ins.resourceManager.GetTexture("test.png");
   }
 
   void GameScene::update(const float dt)
