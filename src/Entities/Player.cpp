63635488854d4b367ba41de7ecd6b699179fc862
Joona Tiinanen <joona_tiinanen@hotmail.com>
2017-07-29 14:55:22 +0300

    Randomizer, player stuff

--- src/Entities/Player.cpp
+++ src/Entities/Player.cpp
@@ -8,24 +8,23 @@ namespace jam
   Player::Player(Instance& ins)
     : Entity(),
       sf::RectangleShape(sf::Vector2f(50.f, 50.f)),
-      m_currentSpeed(250.f),
-      m_jumpAccel(0.f)
+      m_instance(ins),
+      m_currentSpeed(
+        ins.config.float_("PLAYER_START_SPEED"),
+        0.f
+      )
   {
     setFillColor(sf::Color::Red);
   }
 
   void Player::update(const float dt)
   {
-    const float speed = 1000.f * dt;
+    const auto gravity = m_instance.config.float_("GRAVITY");
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-      move(-speed, 0.f);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-      move(speed, 0.f);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-      move(0.f, -speed);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-      move(0.f, speed);
+    //m_currentSpeed.y -= 
+    //m_currentSpeed
+
+    move(m_currentSpeed * dt);
   }
 
   void Player::draw(sf::RenderTarget& target)
